78e394a23d56d1f45e022f6ea4edc469cee4c3c5
Ecalle Thomas <thomasecalle@hotmail.fr>
2019-04-21 01:06:06 +0200

    Use Mockito to mock repositories

--- src/test/java/use_case/PlanInterviewTest.java
+++ src/test/java/use_case/PlanInterviewTest.java
@@ -8,40 +8,60 @@ import common.dto.ProfileDTO;
 import common.response.Response;
 import model.interview.Interview;
 import model.interview.exception.NeitherHalfOrPlainHourStartException;
-import model.interview.exception.NotInWorkingHoursException;
 import org.joda.time.DateTime;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * User: thomasecalle
  * Date: 19/04/2019
  * Time: 16:31
  */
+@ExtendWith(MockitoExtension.class)
+@RunWith(JUnitPlatform.class)
 class PlanInterviewTest {
-    final CandidateReposiory candidateReposiory = id -> new CandidateDTO(
-            new ProfileDTO(new ArrayList<MasteredTechnology>() {{
-                add(new MasteredTechnology(Technology.Android, 3));
-            }}, new ArrayList<Technology>() {{
-                add(Technology.Javascript);
-            }}));
 
-    final ConsultantRecruiterRepository consultantRecruiterRepository = (ConsultantRecruiterRepository) () -> new ArrayList<ConsultantRecruiterDTO>() {{
-        add(new ConsultantRecruiterDTO(
+    private CandidateReposiory candidateReposiory = mock(CandidateReposiory.class);
+
+    private ConsultantRecruiterRepository consultantRecruiterRepository = mock(ConsultantRecruiterRepository.class);
+
+    @Mock
+    InterviewRepository interviewRepository;
+
+    @BeforeEach
+    void before() {
+        when(candidateReposiory.getCandidateById(0)).thenReturn(new CandidateDTO(
                 new ProfileDTO(new ArrayList<MasteredTechnology>() {{
-                    add(new MasteredTechnology(Technology.Android, 8));
+                    add(new MasteredTechnology(Technology.Android, 3));
                 }}, new ArrayList<Technology>() {{
-                    add(Technology.Dotnet);
+                    add(Technology.Javascript);
                 }})));
-    }};
 
-    final InterviewRepository interviewRepository = interview -> {
+        when(consultantRecruiterRepository.getAllConsultantsRecruiter()).thenReturn(
+                new ArrayList<ConsultantRecruiterDTO>() {
+                    {
+                        add(new ConsultantRecruiterDTO(
+                                new ProfileDTO(new ArrayList<MasteredTechnology>() {{
+                                    add(new MasteredTechnology(Technology.Android, 8));
+                                }}, new ArrayList<Technology>() {{
+                                    add(Technology.Dotnet);
+                                }})));
+                    }
+                });
 
-    };
+    }
 
     @Test
     void shouldCreateInterview() {
